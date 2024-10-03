932af217be0036b2eff5873834fe00a3a7ab5e4d
iHonguito <danidawaka@gmail.com>
2024-10-03 08:23:18 -0500

    Settings update for GitHub Pages 5

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import PrincipalPageView from '@/views/PrincipalPage.vue'
+
 const routes = [
   {
-    path : '/',
-    name : 'SourcePage',
-    redirect : '/portfolio'  
+    path: '/',
+    name: 'SourcePage',
+    redirect: '/portfolio'
   },
   {
-    path : '/portfolio',
-    name : 'Portfolio',
-    component : PrincipalPageView
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: PrincipalPageView
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
